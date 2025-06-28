762ad34673f932508e8f124c40c2e957c50e5e58
kumail-alajmi88 <kumailalajmi881@gmail.com>
2025-06-28 18:49:37 +0400

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
-  host: '50.50.50.8', // ← ضع هنا IP السيرفر
+  host: '192.168.2.101', 
   user: 'kumail',
   password: 'Alajmi0011',
   database: 'kam_db'
